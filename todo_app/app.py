90283aea29b4dee0b61bc645cf169e6139d1241b
Josh Powell <josh.powell@softwire.com>
2021-11-03 11:41:45 +0000

    View tasks, create tasks, satisfy pass criteria

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,5 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from .data.session_items import get_items, add_item
 
 from todo_app.flask_config import Config
 
@@ -8,4 +9,10 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    return render_template("index.html", items=items)
+
+@app.route('/create', methods=["POST"])
+def create_task():
+    add_item(request.form.get('task_title'))
+    return redirect('/')
